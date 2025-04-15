24fd640a9e3ccb7ae96b96dc4594efb0b0767c57
thomaslamart <104636880+thomaslamart@users.noreply.github.com>
2025-04-15 21:40:28 +0200

    V0.01

--- experience/experience.js
+++ experience/experience.js
@@ -1,3 +1,21 @@
+function copyToClipboard(element, text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      // Ajouter la classe "copied" pour changer la couleur
+      element.classList.add('copied');
+
+      // Retirer la classe après 1 seconde
+      setTimeout(() => {
+        element.classList.remove('copied');
+      }, 500);
+    })
+    .catch(err => {
+      console.error('Erreur lors de la copie : ', err);
+    });
+}
+
+
+
 function toggleExperienceHK() {
     const experienceDiv = document.querySelector('.div__hk__experience');
     const parentDiv = document.querySelector('.div__hk');
@@ -11,4 +29,19 @@ function toggleExperienceHK() {
     } else {
       parentDiv.classList.add('active');
     }
+  }
+
+    function toggleExperienceArturin() {
+      const experienceDiv = document.querySelector('.div__arturin__experience');
+      const parentDiv = document.querySelector('.div__arturin');
+  
+      // Basculer la classe 'hidden' pour afficher/masquer le contenu
+      experienceDiv.classList.toggle('hidden');
+  
+      // Ajouter ou retirer la classe 'active' sur la div principale
+      if (experienceDiv.classList.contains('hidden')) {
+          parentDiv.classList.remove('active');
+      } else {
+          parentDiv.classList.add('active');
+      }
   }
